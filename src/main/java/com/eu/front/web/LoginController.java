f8001f286fb7f80613bf09e08ed9662d33c5ddfa
821453366 <821453366@qq.com>
2017-10-26 11:04:33 +0800

    完成部分修改

--- src/main/java/com/eu/front/web/LoginController.java
+++ src/main/java/com/eu/front/web/LoginController.java
@@ -1,19 +1,14 @@
 package com.eu.front.web;
 
-import com.eu.front.dto.Result;
 import com.eu.front.entity.Admin;
 import com.eu.front.service.LoginService;
 import com.eu.front.service.UserService;
 import com.eu.front.utils.Constant;
-import com.eu.front.utils.ImgUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
